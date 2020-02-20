77b37855537e640b64e612629a6d38c713aafc33
Eric <you@example.com>
2020-02-20 20:37:05 +0100

    Remove HeaderShown if connected

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Home, Login, Signup, ForgetPassword } from "./container";
+import { Home, Login, Signup, ForgetPassword, Profil } from "./container";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    // const localAccount = AsyncStorage.getItem("account");
+  });
   return (
     <NavigationContainer style={styles.container}>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Home"
+          component={Home}
+        />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Profil" component={Profil} />
+
         <Stack.Screen name="ForgetPassword" component={ForgetPassword} />
         <Stack.Screen name="Connected" component={ForgetPassword} />
       </Stack.Navigator>
